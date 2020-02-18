92662c582487f738769416761499a1b0dc6ac5ca
ildarnarbek <58545811+ildarnarbek@users.noreply.github.com>
2020-02-18 06:05:26 +0400

    update
    styles are divided into reset.css and styles.css.
    js updated with eventlistener for points.
    Added pics and elements for testing slider.
    added links and styles.

--- script.js
+++ script.js
@@ -1,41 +1,75 @@
-let slides = document.querySelectorAll(".slider-container__slide"),
+(function(){
+const slides = document.querySelectorAll(".slider-container__slide"),
   points = document.querySelectorAll(".points-container__points"),
   arrow = document.querySelector(".slider__arrow-container"),
   arrowLeft = document.querySelector(".slider__arrow--left"),
-  arrowRight = document.querySelector(".slider__arrow--right"),
-  currentSlide = 0,
+  arrowRight = document.querySelector(".slider__arrow--right");
+let currentSlide = 0,
   roundCounter = 0;
 
-function changeStyle(elementName, style, n) {
-  return (
-    elementName[currentSlide].classList.remove(style),
-    elementName[currentSlide + n].classList.add(style)
-  );
-}
-function turnArrow(add, remove) {
-  return (
-    add.classList.add("slider__arrow--show"),
-    remove.classList.remove("slider__arrow--show")
-  );
+function changeStyle(array, style, step) {
+  array[currentSlide].classList.remove(style);
+  array[currentSlide + step].classList.add(style);
 }
 
-arrow.onclick = function() {
-    if (roundCounter === 0) {
-      changeStyle(points, "points-container__points--active", 1);
-      changeStyle(slides, "slider-container__slide--show", 1);
-      currentSlide++;
-      if (currentSlide == slides.length - 1) {
-        roundCounter = 1;
-        turnArrow(arrowLeft, arrowRight);
-      }
-    } else {
-      changeStyle(points, "points-container__points--active", -1);
-      changeStyle(slides, "slider-container__slide--show", -1);
-      currentSlide--;
-      if (currentSlide == 0) {
-        roundCounter = 0;
-        turnArrow(arrowRight, arrowLeft);
-      }
+function changeStyleArrow(step) {
+  changeStyle(points, "points-container__points--active", step);
+  changeStyle(slides, "slider-container__slide--show", step);
+  if (step == 1) {
+    currentSlide++;
+  } else {
+    currentSlide--;
+  }
+}
+
+function turnArrow(side) {
+  if (side == "right") {
+    roundCounter = 0;
+    arrowRight.classList.add("slider__arrow--show");
+    arrowLeft.classList.remove("slider__arrow--show");
+  } else {
+    roundCounter = 1;
+    arrowLeft.classList.add("slider__arrow--show");
+    arrowRight.classList.remove("slider__arrow--show");
+  }
+}
+
+arrow.addEventListener("click", function() {
+  if (roundCounter === 0) {
+    changeStyleArrow(1);
+    if (currentSlide == slides.length - 1) {
+      turnArrow("left");
     }
-  };
+  } else {
+    changeStyleArrow(-1);
+    if (currentSlide == 0) {
+      turnArrow("right");
+    }
+  }
+});
 
+let arrayPoints = [];
+
+function pointChangeStyle(e, array, style) {
+  array.forEach(function(item) {
+    item.classList.remove(style);
+  });
+  array[arrayPoints.indexOf(e.target)].classList.add(style);
+}
+
+points.forEach(function(item) {
+  arrayPoints.push(item);
+  item.addEventListener("click", function(e) {
+    pointChangeStyle(e, points, "points-container__points--active");
+    pointChangeStyle(e, slides, "slider-container__slide--show");
+
+    currentSlide = arrayPoints.indexOf(e.target);
+    if (arrayPoints.indexOf(e.target) == 3) {
+      turnArrow("left");
+    }
+    if (arrayPoints.indexOf(e.target) == 0) {
+      turnArrow("right");
+    }
+  });
+});
+})();
